ac122a567056ad86f28bee67273b4b1a86a018d8
BrunaBelo <anabruna28@gmail.com>
2021-02-25 13:36:06 -0300

    adicionar metodo de envio de email

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 class SendMailController {
 
@@ -21,9 +22,9 @@ class SendMailController {
             });
         }
 
-        const surveyAlreadyExists = await surveyRepository.findOne({ id: survey_id })
+        const survey = await surveyRepository.findOne({ id: survey_id })
 
-        if (!surveyAlreadyExists) {
+        if (!survey) {
             return response.status(400).json({
                 error: "Survey does not exists"
             });
@@ -31,9 +32,11 @@ class SendMailController {
 
         const surveyUser = surveysUsersRepository.create({
             user_id: userAlreadyExists.id,
-            survey_id: surveyAlreadyExists.id,
+            survey_id: survey.id,
         });
         await surveysUsersRepository.save(surveyUser);
+
+        await SendMailService.execute(email, survey.title, survey.description);
 
         return response.json(surveyUser);
     }
