6ad7e97e557adf889376dea1ae62c6d1b0e88d4f
BrunaBelo <anabruna28@gmail.com>
2021-02-25 20:23:39 -0300

    refatorar layout para apresentacao de mensagem

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -4,6 +4,7 @@ import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
 import SendMailService from "../services/SendMailService";
+import { resolve } from 'path';
 
 class SendMailController {
 
@@ -14,9 +15,9 @@ class SendMailController {
         const surveyRepository = getCustomRepository(SurveysRepository);
         const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
-        const userAlreadyExists = await userRepository.findOne({ email });
+        const user = await userRepository.findOne({ email });
 
-        if (!userAlreadyExists) {
+        if (!user) {
             return response.status(400).json({
                 error: "User does not exists"
             });
@@ -31,12 +32,20 @@ class SendMailController {
         }
 
         const surveyUser = surveysUsersRepository.create({
-            user_id: userAlreadyExists.id,
+            user_id: user.id,
             survey_id: survey.id,
         });
         await surveysUsersRepository.save(surveyUser);
 
-        await SendMailService.execute(email, survey.title, survey.description);
+        const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+        const variables = {
+            name: user.name,
+            title: survey.title,
+            description: survey.description
+        };
+
+        await SendMailService.execute(email, survey.title, variables, npsPath);
 
         return response.json(surveyUser);
     }
--- src/services/SendMailService.ts
+++ src/services/SendMailService.ts
@@ -1,4 +1,6 @@
-import nodemailer, { Transporter } from 'nodemailer'
+import nodemailer, { Transporter } from 'nodemailer';
+import handlebars from 'handlebars';
+import fs from 'fs';
 
 class SendMailService {
     private client: Transporter;
@@ -19,13 +21,19 @@ class SendMailService {
         });
     }
 
-    async execute(to: string, subject: string, body: string) {
+    async execute(to: string, subject: string, variables: object, path: string) {
+        const templateFileContente = fs.readFileSync(path).toString("utf-8");
+
+        const mailTemplateParse = handlebars.compile(templateFileContente);
+
+        const html = mailTemplateParse(variables);
+
         const message = await this.client.sendMail({
             to,
             subject,
-            html: body,
+            html,
             from: "NPS <noreplay@nps.com.br>"
-        })
+        });
 
         console.log('Message sent: %s', message.messageId);
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
